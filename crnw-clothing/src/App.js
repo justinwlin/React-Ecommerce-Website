b800c65f1a8c05bb66db8bf61fcd2219a5c0468f
Justin Lin <justinwlin@gmail.com>
2019-07-01 19:35:36 -0500

    adding styling and separating into different components from homepage

--- crnw-clothing/src/App.js
+++ crnw-clothing/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./Homepage.component";
+import HomePage from "./pages/homepage/Homepage.component";
 
 function App() {
 	return (
