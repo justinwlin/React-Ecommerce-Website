1079b458db5913263626056f03778d38b7a2c285
Justin Lin <justinwlin@gmail.com>
2019-07-02 00:23:28 -0500

    adding header nav bar

---
+++ crnw-clothing/src/components/header/header.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./header.style.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Header = () => (
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+		</Link>
+		<div className="options" to="/">
+			<Link className="option" to="/shop">
+				SHOP
+			</Link>
+			<Link className="option" to="/shop">
+				CONTRACT
+			</Link>
+		</div>
+	</div>
+);
+
+export default Header;
