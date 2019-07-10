f636d87cf2dd0048c4faf9d793211d5d628b1ced
Justin Lin <justinwlin@gmail.com>
2019-07-10 15:38:30 -0500

    Adding selectors for performance optimization

--- crnw-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crnw-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 import { CustomButton, CartItem } from "../componentIndex";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropDown = ({ cartItems }) => {
 	let cartstuff = cartItems.map((cartItem) => {
@@ -16,8 +17,8 @@ const CartDropDown = ({ cartItems }) => {
 	);
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	cartItems
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropDown);
